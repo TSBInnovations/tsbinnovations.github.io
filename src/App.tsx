ba95017a2165a1e08ba5466d2f22d78c29097c9d
TSB Software Developer <tsbinnovations@gmail.com>
2022-03-30 20:59:18 -0400

    added cra with typescript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="temp header">This page is currently under Construction!</h1>
+      <h3 className="temp sub-header">Please check back later. Thank you.</h3>
+    </div>
+  );
+}
+
+export default App;
