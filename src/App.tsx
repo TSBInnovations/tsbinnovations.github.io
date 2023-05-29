ac5d525e4f0eee76cb3a97b8b0e799acca729c16
TSB Software Developer <tsbinnovations@gmail.com>
2023-05-28 22:12:33 -0400

    updated home page for upcoming tshirt business

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="container">
-      <h1 className="temp header">This page is currently under Construction!</h1>
-      <h3 className="temp sub-header">Please check back later. Thank you.</h3>
+      <h1 className="temp header">TSB Innovations and Our Three Girls is colabing to bring you the latest in fashion</h1>
+      <h3 className="temp sub-header">We will be designing T-Shirts, Hats, Bracelets, Socks, Shoes, Mugs, Plates, and so much more.</h3>
+      <h3 className="temp sub-header">We will be designing T-Shirts, Hats, Bracelets, Socks, Shoes, Mugs, Plates, and so much more.</h3>
+      <h3 className="temp sub-header">Send us your designs and we take care of the rest.</h3>
     </div>
   );
 }
